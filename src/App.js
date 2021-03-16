b9f4c39fb402f63e22f69e689e81a040b59e1829
Ben8842 <z87e99b44r55a@gmail.com>
2021-03-15 22:13:49 -0400

    restructured for bounds definition and component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Building from "./components/Building";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Building sizeValue="20" />
+      </div>
+    );
+  }
 }
+
+//<MyStopwatch />; not ready for this yet?
+//  <div className="HeaderSpot">{inputBox}</div>; also don't need this at this time
 
 export default App;
